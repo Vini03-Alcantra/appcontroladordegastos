0d863c4f766f2eb9aaa57db65442bb51627b4bf4
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-05-03 23:16:49 -0300

    Centralizando título de cada sessão na tela de níveis de consumidores

--- lib/ui/consumista/consumista_page.dart
+++ lib/ui/consumista/consumista_page.dart
@@ -12,25 +12,67 @@ class ConsumistaPage extends StatelessWidget {
           height: altura,
           width: double.infinity,
           color: Colors.redAccent,
-          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text("Consumistas sêniors", style: TextStyle(fontSize: 14),)
+                ))
+            ],
+          ),
         ),
         Container(
           width: double.infinity,
           color: Colors.deepPurple,
           height: altura,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                  child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text("Consumistas pleno", style: TextStyle(fontSize: 14),)
+                ),
+              )
+            ],
+          ),
           
         ),
         Container(
           width: double.infinity,
           color: Colors.deepOrange,
           height: altura,
-          
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                  child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text("Consumistas júnior", style: TextStyle(fontSize: 14))
+                ),
+              )
+            ],
+          ),
         ),
         Container(
           width: double.infinity,
           color: Colors.greenAccent,
           height: altura,
-          
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text("Dados", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
+                )),
+              )],
+          ),
         ),
       ],
     );
