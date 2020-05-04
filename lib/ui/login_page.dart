f5b3a9c20b0548e2515baf06f40d8abb61be2feb
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-05-04 00:38:20 -0300

    Adicionando validações na tela de login

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -12,129 +12,150 @@ class _LoginPageState extends State<LoginPage> {
 
   TextEditingController _controllerUsuario = TextEditingController();
   TextEditingController _controllerPassword = TextEditingController();
-  String _pass;
-  String _user;
-  
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> _formKey = GlobalKey();
     return Scaffold(
       
-      body:  SingleChildScrollView(
-        child: Container(
-          width: double.maxFinite,
-          height: double.maxFinite,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                top: 0,
-                height: MediaQuery.of(context).size.height/2,
-                child: Image.asset("imagens/cidade.jpg"),            
-              ),
-              Positioned(
-                top: 100,
-                left: 32,
-                child: Text("Sign in", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black))
-              ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(          
+          child: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 0,
+                  height: MediaQuery.of(context).size.height/2,
+                  child: Image.asset("imagens/cidade.jpg"),            
+                ),
+                Positioned(
+                  top: 100,
+                  left: 32,
+                  child: Text("Sign in", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black))
+                ),
 
   
-                
-              Positioned(
-                  top: 180,
-                  child: Container(
-                    padding: EdgeInsets.all(32),
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(62),
-                        topRight: Radius.circular(62)
-                      )
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          controller: _controllerUsuario,
-                          decoration: InputDecoration(
-                            hintText: "Username",
-                            prefixIcon: Icon(Icons.person_pin)
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 16, bottom: 60),
-                          child: TextFormField(
-                            obscureText: true,
-                            controller: _controllerPassword,
+                  
+                Positioned(
+                    top: 180,
+                    child: Container(
+                      padding: EdgeInsets.all(32),
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(62),
+                          topRight: Radius.circular(62)
+                        )
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _controllerUsuario,
                             decoration: InputDecoration(
-                              hintText: "Password",
-                              prefixIcon: Icon(Icons.vpn_key),
+                              hintText: "Username",
+                              prefixIcon: Icon(Icons.person_pin)
                             ),
-                          ),
-                        ),
-
-                        Container(
-                          height: 45,
-                          width: double.maxFinite,
-                          decoration: BoxDecoration(
-                            color: Colors.deepOrangeAccent,
-                            borderRadius: BorderRadius.all(Radius.circular(32)),                        
-                          ),
-                          child: RaisedButton(
-                            color: Colors.orange[500],
-                            child: Text("Login", style: TextStyle(color: Colors.white)),
-                            onPressed: (){
-                              if (_controllerUsuario.text == "vinicius" && _controllerPassword.text == "1234") {
-                                Navigator.of(context).push(MaterialPageRoute(builder: (context) => Home()));
+                            validator: (value){
+                              if (value.isEmpty) {
+                                return "Por favor preencher login";
+                              } else {
+                                return null;
                               }
                             },
                           ),
-                        ),
-                        SizedBox(height: 15),
-                        Text("Esqueceu sua senha ?", style: TextStyle(color: Colors.orange[700])),
-
-                        Padding(
-                          padding: const EdgeInsets.only(top: 40, bottom: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                width: 60,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[300],
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(100)
-                                  )
-                                ),
-                                child: Center(child: Icon(Icons.face, color: Colors.orange[500]),
-                                ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 16, bottom: 60),
+                            child: TextFormField(
+                              obscureText: true,
+                              controller: _controllerPassword,
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                prefixIcon: Icon(Icons.vpn_key),
                               ),
-                              Container(
-                                width: 30,
-                              ), 
-                              Container(
-                                width: 60,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey[300],
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(100)
-                                  )
-                                ),
-                                child: Center(
-                                  child: Icon(Icons.fingerprint, color: Colors.orange[500]),
-                                ),
-                              )
-                            ],
+                              validator: (value){
+                                if(value.isEmpty){
+                                  return "Por favor preencher campo de senha";
+                                }else{
+                                  return null;
+                                }
+                              },
+                            ),
                           ),
-                        )                    
-                      ],
-                    ),
-                  ), 
-                )
-            ]
-          )
+
+                          Container(
+                            height: 45,
+                            width: double.maxFinite,
+                            decoration: BoxDecoration(
+                              color: Colors.deepOrangeAccent,
+                              borderRadius: BorderRadius.all(Radius.circular(32)),                        
+                            ),
+                            child: RaisedButton(
+                              color: Colors.orange[500],
+                              child: Text("Login", style: TextStyle(color: Colors.white)),
+                              onPressed: (){
+                                if(_formKey.currentState.validate()){
+                                  Scaffold.of(context).showSnackBar(SnackBar(content: Text("Informações sendo processadas")));
+                                  if (_controllerUsuario.text == "vinicius" && _controllerPassword.text == "1234") {
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => Home()));
+                                }else{
+                                  Scaffold.of(context).showSnackBar(SnackBar(content: Text("Login ou senha errados")));
+                                }
+                                }
+                                
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Text("Esqueceu sua senha ?", style: TextStyle(color: Colors.orange[700])),
+
+                          Padding(
+                            padding: const EdgeInsets.only(top: 40, bottom: 40),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(100)
+                                    )
+                                  ),
+                                  child: Center(child: Icon(Icons.face, color: Colors.orange[500]),
+                                  ),
+                                ),
+                                Container(
+                                  width: 30,
+                                ), 
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(100)
+                                    )
+                                  ),
+                                  child: Center(
+                                    child: Icon(Icons.fingerprint, color: Colors.orange[500]),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )                    
+                        ],
+                      ),
+                    ), 
+                  )
+              ]
+            )
+          ),
         ) 
       )          
     );
